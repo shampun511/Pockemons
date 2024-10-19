e11969295baf85a5da2ca77e75a1f24be09c20dd
shampun511 <den-belov00@mail.ru>
2024-10-19 18:07:16 +0500

    first commit

---
+++ logic.py
@@ -0,0 +1,58 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.color = self.get_color( )
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']['front_default'])
+        else:
+            return 'https://upload.wikimedia.org/wikipedia/ru/7/77/Pikachu.png'
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+    
+    #метод для цвета
+    def get_color(self):
+        url = f'https://pokeapi.co/api/v2/pokemon-species/{self.pokemon_number}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['color']['name'])
+        else:
+            return "Pikachu"
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+
+    def show_color(self):
+        return (f'Цвет твоего покемона: {self.color}')
+
+
